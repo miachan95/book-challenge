51d7a3b185a647906198dba09847283e381202a0
trangltm <trangltm@kaopiz.com>
2021-05-26 11:19:05 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/Book_community.png" width="25%" />
+    <Decode challengeNum="1" />
+  </div>
+</template>
+
+<script>
+import Decode from "./components/Decode";
+
+export default {
+  name: "App",
+  components: {
+    Decode,
+  },
+  props: {
+    challengeCode: String,
+    challengeNum: String,
+  },
+};
+</script>
+
+<style>
+html {
+  background: #fff;
+}
+#app {
+  font-family: "Source Code Pro", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #E18D96;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Decode.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>Ice Cream Challenge {{ nowNum }}</h1>
+    <div class="col">
+      <form id="code" class="form-group">
+        <div class="form-group">
+          {{ msg }}
+          <input
+            type="code"
+            v-model="submitCode"
+            class="form-control"
+            id="exampleInputEmail1"
+            placeholder="Mã bí mật"
+          />
+        </div>
+        <button type="" @click="submit" class="btn btn-danger">Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Decode",
+  data: () => ({
+    nowNum: 1,
+    msg: "",
+    dataCode: [
+      { challengeNum: 1, code: "ABC" },
+      { challengeNum: 2, code: "AAA" },
+      { challengeNum: 3, code: "BBB" },
+      { challengeNum: 4, code: "DDD" },
+      { challengeNum: 5, code: "ALAL" },
+    ],
+  }),
+  methods: {
+    submit() {
+      this.nowNum = this.nowNum + 1;
+      if (
+        this.dataCode.find(this.checkCode(code)) &&
+        this.dataCode.isFinite((item) => item.challengeNum === this.nowNum)
+      ) {
+        this.msg = "Thành công";
+      } else {
+        this.msg = "Sai rồi :)";
+      }
+    },
+    checkCode(code) {
+      return this.submitCode === code;
+    },
+    checkChalNum() {},
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
